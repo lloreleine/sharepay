d60254fa506e1f40a4c06ed8e64449e7c6388723
Maxime Delplanque <maxime.delplanque@decathlon.com>
2018-01-02 16:34:36 +0100

    Add serverjs

---
+++ server.js
@@ -0,0 +1,27 @@
+const PG = require("pg");
+const fetch = require("node-fetch");
+
+const express = require("express");
+const nunjucks = require("nunjucks");
+const app = express();
+
+const port = process.env["PORT"] || 3000;
+
+nunjucks.configure("views", {
+  autoescape:true,
+  express:app
+});
+
+app.use(express.static('./images/'));
+app.use(require("body-parser").urlencoded({ extended: true }));
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "njk");
+
+app.get("/", function(request, result) {
+  //to complete
+});
+
+app.listen(port, function () {
+  console.log("Server listening on port:" + port);
+});
