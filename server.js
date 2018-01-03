eb3e232a2bbb0684df6c77b52af3e97b96527364
Loreleine Le Clerc <loreleine.leclerc@decathlon.com>
2018-01-03 12:23:14 +0100

    first glimpse of the activity view page

--- server.js
+++ server.js
@@ -18,6 +18,17 @@ app.use(require("body-parser").urlencoded({ extended: true }));
 app.set("views", __dirname + "/views");
 app.set("view engine", "njk");
 
+<<<<<<< HEAD
+=======
+app.get("/", function(request, result) {
+  //to complete
+});
+
+app.get("/view_activity", function(request, result) {
+  result.render("view_activity");
+});
+
+>>>>>>> first glimpse of the activity view page
 app.listen(port, function () {
   console.log("Server listening on port:" + port);
 });
